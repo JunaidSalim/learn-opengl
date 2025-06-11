9f886b303e1222ab8c27980008c0b05eae8afcfa
Junaid Saleem <junaidsaleem986@gmail.com>
2025-06-11 14:39:22 +0500

    shraders In/Out

--- apps/src/rectangle.cpp
+++ apps/src/rectangle.cpp
@@ -9,20 +9,23 @@ const int WIN_HEIGHT = 600;
 const char* vertexShaderSource = R"(
 #version 330 core
 layout (location = 0) in vec3 aPos;
+out vec4 tColor;
 
 void main()
 {
     gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    tColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);
 }
 )";
 
 const char* fragmentShaderSource = R"(
 #version 330 core
 out vec4 fColor;
+in vec4 tColor;
 
 void main()
 {
-    fColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    fColor = tColor;
 }
 )";
 
