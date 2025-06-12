fdcf823516ad09df976ff13b6c4db3cdd5a7652c
Junaid Saleem <junaidsaleem986@gmail.com>
2025-06-12 15:47:12 +0500

    fixed previous codes

--- apps/src/texture/texture.cpp
+++ apps/src/texture/texture.cpp
@@ -35,29 +35,13 @@ int main() {
 
   Shader ourShader("shader.vs", "shader.fs");
 
-  //   float vertices[] = {
-  //       // positions          // colors           // texture coords
-  //       0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top right
-  //       0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
-  //       -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom left
-  //       -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
-  //   };
-
   float vertices[] = {
-      // positions          // colors           // texture coords
-      0.1f,  0.1f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top right
-      0.1f,  -0.1f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
-      -0.1f, -0.1f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom left
-      -0.1f, 0.1f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
+      // positions   // texture coords
+      0.1f,  0.1f,  0.0f, 1.0f, 1.0f,  // top right
+      0.1f,  -0.1f, 0.0f, 1.0f, 0.0f,  // bottom right
+      -0.1f, -0.1f, 0.0f, 0.0f, 0.0f,  // bottom left
+      -0.1f, 0.1f,  0.0f, 0.0f, 1.0f   // top left
   };
-
-  //   float vertices[] = {
-  //       // positions        // colors         // texture coords (inverted)
-  //       0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,  // top right
-  //       0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,  // bottom right
-  //       -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,  // bottom left
-  //       -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f   // top left
-  //   };
 
   unsigned int indices[] = {
       0, 1, 3,  // first triangle
@@ -75,15 +59,12 @@ int main() {
 
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
-  // color attribute
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
+
   // texture coord attribute
-  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-  glEnableVertexAttribArray(2);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   unsigned int texture;
   glGenTextures(1, &texture);
