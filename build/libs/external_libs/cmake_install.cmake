0d636f7edd0923704c9a9bcfc118fbe41cb2d260
Junaid Saleem <junaidsaleem986@gmail.com>
2025-06-12 10:19:37 +0500

    done with transformations

--- build/libs/external_libs/cmake_install.cmake
+++ build/libs/external_libs/cmake_install.cmake
@@ -38,18 +38,11 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
+  # Include the install script for each subdirectory.
   include("C:/Users/JS/Desktop/OpenGL_Game/build/libs/external_libs/glad/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
   include("C:/Users/JS/Desktop/OpenGL_Game/build/libs/external_libs/glfw-3.4/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
   include("C:/Users/JS/Desktop/OpenGL_Game/build/libs/external_libs/stb_image/cmake_install.cmake")
+
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
