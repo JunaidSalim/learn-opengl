b62fb750eada3673c10e6a951f0cf4fd4b407e91
Junaid Saleem <junaidsaleem986@gmail.com>
2025-06-13 09:55:07 +0500

    coordinate basic done and working

--- libs/internal_libs/shaders/include/shader_s.h
+++ libs/internal_libs/shaders/include/shader_s.h
@@ -2,6 +2,7 @@
 #define SHADER_H
 
 #include <glad/glad.h>  // include glad to get the required OpenGL headers
+#include <glm/glm.hpp>
 
 #include <string>
 #include <fstream>
@@ -12,14 +13,18 @@ class Shader {
  public:
   // the program ID
   unsigned int ID;
+
   // constructor reads and builds the shader
   Shader(const char *vertexPath, const char *fragmentPath);
+
   // use/activate the shader
   void use();
+
   // utility uniform functions
   void setBool(const std::string &name, bool value) const;
   void setInt(const std::string &name, int value) const;
   void setFloat(const std::string &name, float value) const;
+  void setMat4(const std::string &name, const glm::mat4 &mat) const;
 
  private:
   void checkCompileErrors(unsigned int shader, std::string type);
